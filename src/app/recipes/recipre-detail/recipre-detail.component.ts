9796758e3f05ce10de17c647a1a1722260f9d19d
Kloud333 <klekotvr@gmail.com>
2020-01-27 01:45:22 +0200

    - created components

---
+++ src/app/recipes/recipre-detail/recipre-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipre-detail',
+  templateUrl: './recipre-detail.component.html',
+  styleUrls: ['./recipre-detail.component.css']
+})
+export class RecipreDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
